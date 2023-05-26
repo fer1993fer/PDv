0e3119b9761f46bcecc57eec1b9a5a666d362596
fer1993fer <115002535+fer1993fer@users.noreply.github.com>
2023-05-26 10:09:56 -0700

    Create como_usar __main__.py
    asi es como se usa con la GUI

---
+++ como_usar __main__.py
@@ -0,0 +1,12 @@
+Lo primero:
+  crear un entorno virtual e ingresar
+  "python3 -m venv app1"
+  entramos a la carpeta app1 
+  "cd app1"
+  para ingresar al entorno virtual ejecuta lo siguiente
+  "source bin/activate"
+  coopia o mueve el archivo requirements.txt dentro de la carpeta app1 y ejecuta
+  "pip3 install -r requirements.txt"
+  ahora mueve el main.py y el __main__.py dentro de la carpeta app1 y ejecuta
+  "pithon3 __main__py"
+  
